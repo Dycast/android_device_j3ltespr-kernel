757971120225ae6c94de2d8ccef724c6c9d1de30
Grace5921 <androidlover5842@gmail.com>
2016-11-02 12:57:12 +0530

    update readme

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,12 @@
 add_lunch_combo cm_j3ltespr-userdebug
 add_lunch_combo cm_j3ltespr-eng
 
+function j3ltespr_boot
+{
+lunch cm_j3ltespr-userdebug
+make -j4 bootimage
+}
+
 function j3ltespr_zip
 {
 rsync -r device/samsung/j3ltespr/META-INF out/target/product/j3ltespr/META-INF
